dbb53bcb9dccfb7fc34894e1e8f8a869bdd32dfd
KAthousand <KAthousand@gmail.com>
2021-05-05 12:49:46 -0400

    basic header and home card setup

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Home.css'
 
 function Home(props) {
   return (
-    <div>
-      <h1>I am the home component</h1>
+    <div className='home-container'>
+      <div className='address-container'>
+        <p>1234 Address St, Detroit, MI</p>
+      </div>
+      <div className='title-photo'></div>
+      <div className='about-container'>
+        <div className='about-text'></div>
+      </div>
     </div>
   );
 }
--- client/src/screens/shared/Header/Header.jsx
+++ client/src/screens/shared/Header/Header.jsx
@@ -5,7 +5,15 @@ function Header(props) {
   return (
     <div className='header-container'>
       <div className='header-content'>
-        <a href='_blank'>MENU</a>
+        <a href='_blank' className='nav-link'>ORDER ONLINE</a>
+        <a href='/menu' className='nav-link'>MENU</a>
+        <a href='/info' className='nav-link'>LOCATION &amp; HOURS</a>
+        <a href='_blank' className='nav-link'>PRIVATE EVENTS</a>
+        <a href='_blank' className='nav-link'>RESERVATIONS</a>
+      </div>
+      <div className='nav-menu'>
+        <div className='logo'>LOGO</div>
+        <button>X</button>
       </div>
     </div>
   );
